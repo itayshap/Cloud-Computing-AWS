079a6fc1e96011598e5c3f488fc657f343251577
avivples <aviv.ples1@gmail.com>
2022-06-06 14:33:54 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -23,15 +23,15 @@ MY_IP=$(curl ipinfo.io/ip)
 echo "My IP: $MY_IP"
 
 
-echo "setup rule allowing SSH access to $MY_IP only"
-aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 22 --protocol tcp \
-    --cidr $MY_IP/32
+# echo "setup rule allowing SSH access to $MY_IP only"
+# aws ec2 authorize-security-group-ingress        \
+#     --group-name $SEC_GRP --port 22 --protocol tcp \
+#     --cidr $MY_IP/32
 
-echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
-aws ec2 authorize-security-group-ingress        \
-    --group-name $SEC_GRP --port 5000 --protocol tcp \
-    --cidr $MY_IP/32
+# echo "setup rule allowing HTTP (port 5000) access to $MY_IP only"
+# aws ec2 authorize-security-group-ingress        \
+#     --group-name $SEC_GRP --port 5000 --protocol tcp \
+#     --cidr $MY_IP/32
 
 
 SEC_GRP_QM="myQM-sg-`date +'%N'`"
