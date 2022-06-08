36169906318b9105214094c5a8b52797eef85a1c
avivples <aviv.ples1@gmail.com>
2022-06-09 02:39:54 +0300

    Update QM_app.py

--- QM_app.py
+++ QM_app.py
@@ -1,15 +1,18 @@
 import boto3
+import http
 import requests
 import json
 from flask import Flask, request, jsonify
 import time 
 import threading
+from multiprocessing import Value
 import queue
 from datetime import datetime
 
 AUTO_SCALE_TIME = 3
-thread_lock = threading.Lock()
-work_id = 0
+sem = threading.Semaphore()
+work_id = Value('i', 0)
+worker_counter = Value('i', 0)
 
 config = {
     "DEBUG": True  # run app in debug mode
@@ -20,39 +23,43 @@ public_ip = requests.get("http://169.254.169.254/latest/meta-data/public-ipv4").
 def spawn_worker():
     ec2 = boto3.client('ec2', region_name='us-east-1')
     user_data = f'''#!/bin/bash
-    cat > /home/ubuntu/app.py<< EOF
-    import requests
-    import os
-    import time
-    def work(buffer, iterations): 
-        import hashlib 
-        output = hashlib.sha512(buffer).digest() 
-        for i in range(iterations - 1): 
-            output = hashlib.sha512(output).digest() 
-        return output
+cat << EOF > /home/ubuntu/app.py
+import requests
+import os
+import time
+import http
 
-    SECONDS_TO_TERMINATE = 5
-    start_time = time.time()  
+def work(buffer, iterations):
+    import hashlib
+    output = hashlib.sha512(buffer).digest()
+    for i in range(iterations - 1):
+        output = hashlib.sha512(output).digest()
+    return output
 
-    while True:
-        response = requests.get("http://{public_ip}:5000/get_work")
-        if response is None and int(time.time() - start_time) > SECONDS_TO_TERMINATE:
-            break
-        else:
-            work_data = response.get_json()
-            iterations = work_data["iterations"]
-            buffer = work_data["work"]
-            completed = work(buffer, iterations)
-            response = requests.put("http://{public_ip}:5000/send_work", json={"worker_id": work_data["worker_id"], "work": str(completed)})
-            start_time = time.time()
-    os.system('sudo shutdown -h now')
-    EOF
+SECONDS_TO_TERMINATE = 5
+start_time = time.time()
 
-    sudo apt update
-    cd /home/ubuntu/
-    python3 app.py
+while True:
+    response = requests.get("http://34.228.157.101:5000/get_work")
+    if response.status_code == http.HTTPStatus.NO_CONTENT:
+		if int(time.time() - start_time) > SECONDS_TO_TERMINATE:
+			break
+		else:
+			continue
+    else:
+		work_data = response.json()
+		iterations = work_data["iterations"]
+		buffer = work_data["work"].encode('utf-8')
+		completed = work(buffer, iterations)
+		response = requests.put("http://34.228.157.101:5000/send_work", data=f'{work_data["work_id"]}: {completed}')
+		start_time = time.time()
+os.system('sudo shutdown -h now')
+EOF
 
-    '''
+sudo apt update
+cd /home/ubuntu/
+python3 app.py
+'''
 
     instance = ec2.run_instances(ImageId='ami-042e8287309f5df03', MinCount=1, MaxCount=1, UserData=user_data, 
     InstanceType='t2.micro', InstanceInitiatedShutdownBehavior = 'terminate')
@@ -60,13 +67,21 @@ def spawn_worker():
 
 def load_balancing():
     while True:
-        with thread_lock:
-            workers = work_queue.queue
-            if len(workers) > 0:
-                middle_work = workers[int(len(workers) / 2)]
-                if int(time.time() - middle_work['time']) > AUTO_SCALE_TIME:
-                    spawn_worker()
-        time.sleep(5)
+        sem.acquire()
+        workers = work_queue.queue
+        if len(workers) > 0:
+            with worker_counter.get_lock():
+                if worker_counter >= len(workers):
+                    sem.release()
+                    time.sleep(2)
+                    continue
+            middle_work = workers[int(len(workers) / 2)]
+            if int(time.time() - middle_work['time']) > AUTO_SCALE_TIME:
+                spawn_worker()
+                with worker_counter.get_lock():
+                    worker_counter.value += 1
+        sem.release()                
+        time.sleep(2)
 
 app = Flask(__name__)
 work_queue = queue.Queue()
@@ -77,34 +92,39 @@ app.config.from_mapping(config)
 def enqueue():
     iterations = int(request.args.get('iterations'))
     work = request.get_data()
-    new_work = {'work_id' :work_id, 'time': time.time(), 'work': work, 'iterations' : iterations}
+    with work_id.get_lock(): 
+        new_work = {'work_id' :work_id.value, 'time': time.time(), 'work': work, 'iterations' : iterations}
     work_queue.put(new_work)
-    with thread_lock:
-        work_id += 1 
+    with work_id.get_lock():
+        work_id.value += 1
     return 'the work is in queue'
 
 @app.route('/pullCompleted', methods=['POST'])
 def pullCompleted():
     top = int(request.args.get('top'))
-    completed_jobs = {}
-    for i in range(top):
+    completed_jobs = []
+    for _ in range(top):
         if completed_work.empty():
             break
-        completed_jobs.update(completed_work.get())
-    return jsonify(completed_jobs)
+        completed_jobs.append(completed_work.get())
+    if len(completed_jobs) == 0:
+        return "No completed work available"
+    return ('\n').join(completed_jobs)
 
 
 @app.route('/get_work')
 def get_work():
     if work_queue.empty():
-        return None
+        return ('', http.HTTPStatus.NO_CONTENT)
     else:
-        return work_queue.get()
+        return jsonify(work_queue.get())
 
 @app.route('/send_work', methods=['PUT'])
 def send_work():
-    work = request.get_json()
+    work = request.get_data().decode('utf-8')
     completed_work.put(work)
+    with worker_counter.get_lock():
+        worker_counter.value -= 1
     return "work submitted"
 
 @app.route('/', methods=['GET'])
@@ -114,3 +134,4 @@ def status():
 
 if __name__ == "__main__":
     threading.Thread(target=load_balancing).start()
+    app.run(host='0.0.0.0')
